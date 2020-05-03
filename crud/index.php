3b2913b4582bffdd2081b982c752d92752bf2d8f
widiantariputri <ayutari16@gmail.com>
2020-05-04 00:31:18 +0800

    tugas pagination

--- crud/index.php
+++ crud/index.php
@@ -2,9 +2,18 @@
 require 'function.php';
 if (isset($_GET["aw"])) {
     $key = $_GET["aw"];
+    $page = 0;
     $mahasiswa = query("SELECT * FROM mahasiswa WHERE nama LIKE '%$key%' OR nim LIKE '%$key%' OR email LIKE '%$key%' OR fakultas LIKE '%$key%' ORDER BY nim");
 } else {
-    $mahasiswa = query("SELECT * FROM mahasiswa ORDER BY nim");
+    $halaman = 5; /* page halaman*/
+    $page    =isset($_GET["halaman"]) ? (int)$_GET["halaman"] : 1;
+    $mulai    =($page>1) ? ($page * $halaman) - $halaman : 0;
+    $result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+    $total = mysqli_num_rows($result);
+    $pages = ceil($total/$halaman);  
+    $sql = "SELECT * FROM mahasiswa ORDER BY nim LIMIT $mulai,$halaman";
+    $mahasiswa = query($sql);          
+    $no =$mulai+1;
 }
 ?>
 
@@ -15,7 +24,7 @@ if (isset($_GET["aw"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SIMAK</title>
+    <title>PAGINATION</title>
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
@@ -25,12 +34,10 @@ if (isset($_GET["aw"])) {
     <div class="container">
         <div class="table-wrapper">
             <div class="table-title cf">
-                <div class="title-row">
-                    <h2>Daftar <b>Mahasiswa<b></h2>
-                </div>
-                <div class="btn-row">
+                <h2>Daftar Mahasiswa</h2>
+                <!-- <div class="btn-row">
                     <a class="btn-add bg-green" href="add.php"><i class="fas fa-plus-square fa-lg"></i>Tambah Data Mahasiswa</a>
-                </div>
+                </div> -->
             </div>
             <div class="search-wrapper">
                 <div class="field">
@@ -68,16 +75,17 @@ if (isset($_GET["aw"])) {
                                 <td><?= $row["tglLahir"] ?></td>
                                 <td><?= $row["email"] ?></td>
                                 <td><?= $row["fakultas"] ?></td>
-                                <td>
-                                    <a href="update.php?id=<?= $row["idmahasiswa"] ?>" class="edit"><i class="far fa-edit fa-lg"></i></a>
-                                    <a href="delete.php?id=<?= $row["idmahasiswa"] ?>" onclick="return confirm('Hapus Data?');" class="delete"><i class="fas fa-trash-alt fa-lg"></i></a>
-                                </td>
                             </tr>
                         <?php $i++;
                         endforeach;
                         ?>
                     </tbody>
                 </table>
+            </div>
+            <div class="pagination">
+                <?php for ($i=1; $i<=$pages ; $i++){ ?>
+                   <a href="?halaman=<?php echo $i; ?>"><?php echo $i; ?></a>
+                <?php } ?>
             </div>
         </div>
     </div>
